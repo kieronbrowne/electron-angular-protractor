a7faaf83385f83a916987821a1c981f279b0e06c
Kieron Browne <kieron.browne@totalobjects.co.uk>
2016-05-13 15:47:26 +0100

    'require' lodash, to demonstrate node within the browser

--- app/app.js
+++ app/app.js
@@ -1,7 +1,12 @@
 'use strict';
 
+console.warn('__dirname: ' + __dirname);
+
+const _ = nodeRequire('lodash');
+const msg = 'hello, world!';
+
 let app = angular.module('app', []);
 
 app.component('main', {
-    template: '<h1>Hello, world!</h1>'
+    template: `<h1>${_.capitalize(msg)}</h1>`
 });
